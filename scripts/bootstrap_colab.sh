78e421da558de401cb5d569481f835313b74e740
Erik Owen <eowen@wi.mit.edu>
2026-01-04 16:17:19 -0500

    Download toy data

--- scripts/bootstrap_colab.sh
+++ scripts/bootstrap_colab.sh
@@ -1,20 +1,34 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Installing tools..."
+echo "> Installing tools..."
 sudo apt-get -qq update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y \
   bowtie2 samtools bcftools tabix pigz
-
-# echo "Downloading workshop data..."
-# mkdir -p data/loci
-# BASE="https://raw.githubusercontent.com/erik-owen/2026_QMW_Genomics/main/data/loci"
-# for f in locus1.fa locus1.truth.vcf locus1.reads_R1.fastq.gz; do
-#   curl -L -o "data/loci/$f" "$BASE/$f"
-# done
+echo "Tools installed."
 
 echo "Tool versions:"
 bt2_ver="$(bowtie2 --version 2>&1 || true)"
 echo "${bt2_ver%%$'\n'*}"          # first line, no SIGPIPE
 samtools --version | head -n 1 || true
 bcftools --version | head -n 1 || true
+
+echo "> Downloading workshop data..."
+!mkdir -p data/loci
+BASE="https://raw.githubusercontent.com/erik-owen/2026_QMW_Genomics/main/data/loci"
+for f in \
+  locus1_hg38_chr11_5215464_5237071_HBB.fa \
+  locus1_hg38_chr11_5215464_5237071_HBB.repeats.local.bed \
+  locus1_HBB.ind1_noHbS.reads_R1.fastq.gz \
+  locus1_HBB.ind1_noHbS.truth.vcf \
+  locus1_HBB.ind2_HbS_carrier.reads_R1.fastq.gz \
+  locus1_HBB.ind2_HbS_carrier.truth.vcf \
+  locus2_hg38_chr8_96243643_96263643_MTERF3.fa \
+  locus2_hg38_chr8_96243643_96263643_MTERF3.repeats.local.bed \
+  locus2_MTERF3_repeat.ind1_repeat_mix.reads_R1.fastq.gz \
+  locus2_MTERF3_repeat.ind1_repeat_mix.truth.vcf \
+; do
+  curl -L -o "data/loci/$f" "$BASE/$f"
+done
+!ls -lh data/loci | sed 's/^/  /'
+
